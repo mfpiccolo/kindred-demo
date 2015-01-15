90e4acde9a8c3067da1074e3aff90f9a74ac093c
Mike Piccolo <mfpiccolo@gmail.com>
2015-01-14 22:13:26 -0800

    Add save all nested test

--- test/integration/nested_invoice_integration_test.rb
+++ test/integration/nested_invoice_integration_test.rb
@@ -10,6 +10,16 @@ describe "Nested Invoice Kindred Integration", type: :feature, js: true do
     visit "/nested_invoices/#{@invoice.id}/edit"
     click_link "+ New Box"
     click_link "New Box Line Item"
-    save_and_open_page
+    click_link "Save All"
+    sleep 0.1
+    page.evaluate_script('jQuery.active == 0').must_equal true
+    page.all(".error").count.must_equal(1)
+    all('input').each { |i| i.set("1") }
+    BoxLineItem.count.must_equal 0
+    click_link 'Save All'
+    sleep 0.1
+    page.evaluate_script('jQuery.active == 0').must_equal true
+    page.all(".error").count.must_equal(0)
+    BoxLineItem.count.must_equal 1
   end
 end
